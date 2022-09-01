2721499dea45f1034e8ee1897c153f0c9219b53a
Bob Ghosh <bob.ghosh@sapiens.com>
2022-09-01 16:55:05 +0530

    commit6

---
+++ DecisionNG/DecisionNG_Automation/Script/Export_Decision.js
@@ -0,0 +1,52 @@
+﻿//USEUNIT SelectingOptionfromDropDown_Role
+
+function Export_Decision(typeOfExport,fileFormat,fileNameWithExt)
+{
+  filePath = "C:\\Users\\"+Sys.UserName+"\\Downloads\\"+fileNameWithExt
+  if(aqFile.Exists(filePath))
+  {
+    aqFile.Delete(filePath)
+  }
+  let page = Aliases.browser.pageSapiensDecision2
+  page.FindElement("//div[@type='button']").Click()
+  page.WaitElement("//a[contains(., 'export')]",5000).HoverMouse()
+  switch(aqString.ToLower(typeOfExport))
+  {
+    case "asset":
+    page.WaitElement("//a[contains(., 'Asset')]", 5000).Click()
+    page.WaitElement("//dcn-combo-box[@name='Exported File Format']//span[@class='wj-input-group-btn']", 5000).Click()
+    SelectingOptionfromDropdown(fileFormat,"No")
+    page.FindElement("//button[contains(text(), 'Export')]").Click()
+    aqObject.CheckProperty(page.WaitElement("//div[contains(@class, 'toast-info')]", 25000), "contentText", cmpEqual, "Export process has started and may take a while to complete");
+    page.WaitElement("//div[contains(@class, 'toast-info')]", 25000).Click()
+    break;
+  }
+  
+  WaitForFileToDownloadInDownloadsFolder(fileNameWithExt)
+  if(aqFile.Exists("C:\\Users\\"+Sys.UserName+"\\Downloads\\"+fileNameWithExt))
+  {
+    Log.Checkpoint("Export is successful")
+  }
+  else
+    Log.Error("Export is not successful")
+
+}
+
+function WaitForFileToDownloadInDownloadsFolder(fullFileName){
+  
+  var flag = false
+  filePath = "C:\\Users\\"+Sys.UserName+"\\Downloads\\"+fullFileName;
+  
+  while(flag == false){
+    
+    if(aqFile.Exists(filePath)){
+      flag = true;
+      Log.Checkpoint(fullFileName+' file is now downloaded successfully')
+    }
+    else{
+      flag = false;
+      Log.Message('waiting for file to download');
+    }
+
+  }
+}
---
+++ DecisionNG/DecisionNG_Automation/Script/Import_Decision.js
@@ -0,0 +1,27 @@
+﻿//USEUNIT RefLibrary
+
+function Import_New_Decision(){
+  
+  let page = Aliases.browser.pageSapiensDecision2
+  let importDcnBtn = page.FindElement(ORTasks.dcnImportDecision+'//button');
+  
+  importDcnBtn.click()
+  
+  page.FindElement(ORGeneric.dcnComboBox+'//button').WaitProperty('Enabled', true, 10000)
+  page.FindElement(ORGeneric.dcnComboBox+'//button').Click();
+  SelectingOptionfromDropDown_Role.SelectingOptionfromDropdown('JSON',"No")
+  
+  //choose file
+  page.FindElement("//*[@value='Browse']").Click();
+  filePath = 'C:\Users\Bob.ghosh\Downloads\Tuition Fee Loan Eligibility__.zip'
+  Aliases.browser.WaitAliasChild("dlgOpen", 10000).cbxFileName.ComboBox.SetText(filePath)
+}
+
+function kk(){
+  
+//let page = Aliases.browser.pageSapiensDecision2
+//page.FindElement("//*[@value='Browse']").Click();
+//filePath = 'C:\\Users\\Bob.ghosh\\Downloads\\Tuition Fee Loan Eligibility__.zip'
+//  Aliases.browser.WaitAliasChild("dlgOpen", 10000).cbxFileName.ComboBox.SetText(filePath)
+  Sys.HighlightObject(Aliases.browser.dlgOpen.btnOpen)
+}
